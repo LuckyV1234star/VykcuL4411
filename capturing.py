fec748dcb880612fb99a7d7e00b2b616aa054dd3
LuckyV1234star <luckyv1234star@gmail.com>
2024-11-06 16:28:00 +0800

    capturing image done

--- capturing.py
+++ capturing.py
@@ -4,12 +4,12 @@ import threading
 
 
 cam_port = 0
-cam = cv2.VideoCapture(cam_port)
-
+cam = cv2.VideoCapture(cam_port, cv2.CAP_DSHOW)
 
 result, image = cam.read()
 
 if result:
-    time.localtime
-    cv2.imwrite(f"{time.asctime(time.localtime())}.jpg", image)
-    
+    cv2.imshow(f"{time.asctime(time.localtime())}", image)
+    cv2.imwrite(f"\\img\\{time.asctime(time.localtime())}.jpg", image)
+
+    cv2.waitKey(0)
