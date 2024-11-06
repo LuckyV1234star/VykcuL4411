e874da65cbc9e930c31f025ef6bb6431e8479014
LuckyV1234star <luckyv1234star@gmail.com>
2024-11-07 00:49:38 +0800

    execute part

--- counting.py
+++ counting.py
@@ -1,9 +1,17 @@
 import os
+import time
 
-def count():
+def count(flight = "CX 524"): #assume a flight input
     plastic = len(os.listdir("./img/plastic"))
     can = len(os.listdir("./img/can"))
     tetra_pak = len(os.listdir("./img/tetra_pak"))
     other = len(os.listdir("./img/other"))
 
+    for i in os.listdir("./img"):
+        for j in os.listdir(f"./img/{i}"):
+            os.remove(f"./img/{i}/{j}")
+
+    file = open(f"./record/{flight} {time.strftime("%Y%m%d%H%M%S")}.txt", "w")
+    file.write(f"Plastic:{plastic} Can:{can} Tetra:{tetra_pak} Other:{other}")
+    
     return plastic, can, tetra_pak, other
