507cbd4f85f57afb12efe5d19e2fcd51f3c2bfe8
LuckyV2 <luckyv1234star>
2024-11-06 22:07:42 +0800

    debuged capturing

--- capturing.py
+++ capturing.py
@@ -6,8 +6,10 @@ cam = cv2.VideoCapture(cam_port, cv2.CAP_DSHOW)
 
 def capture():
     result, image = cam.read()
-    file_name = f"\\img\\temp\\{time.asctime(time.localtime())}.jpg"
+    file_name = f"{time.strftime("%Y%m%d%H%M%S")}.jpg"
 
     if result:
-        cv2.imwrite(file_name, image)
+        
+        cv2.imwrite(f"./img/temp/{file_name}", image)
+        print(file_name)
         return file_name
